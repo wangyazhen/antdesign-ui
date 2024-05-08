dd517b5e7fda9cb3ecf1763fadcfc7195ff4bdd2
wangyazhen <603684240@qq.com>
2024-05-08 14:28:04 +0800

    feat: remove search select

--- components/index.tsx
+++ components/index.tsx
@@ -2,14 +2,12 @@
  * @Author: 王亚振
  * @Date: 2020-10-13 14:59:23
  * @LastEditors: 王亚振
- * @LastEditTime: 2024-05-08 09:54:43
+ * @LastEditTime: 2024-05-08 10:53:50
  * @FilePath: /antdesign-ui/components/index.tsx
  */
 export { default as BaseSelect } from "./base-select";
 
 export { default as Button } from "./Button";
-
-export { default as SearchSelect } from "./search-select";
 
 export { default as ReactList } from "./react-list";
 
